3eb348d0898d58538b71165332112973cdb3cb5b
Carlos Arteaga <carlos.arteaga@bancoazteca.com>
2020-07-09 17:26:31 -0500

    changes

---
+++ DogsJetPack/app/src/main/java/com/bancoazteca/dogsjetpack/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bancoazteca.dogsjetpack.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bancoazteca.dogsjetpack.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
