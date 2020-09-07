67f5e7573db420de5e91133a30e07f8624e8ac4e
Carlos Arteaga <carlos.arteaga@bancoazteca.com>
2020-09-07 17:21:24 -0500

    add new proyect rick and morty

---
+++ RickAndMortiMVVM/app/src/main/java/com/charlye934/rickandmorty/ui/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.charlye934.rickandmorty.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.charlye934.rickandmorty.R
+import com.charlye934.rickandmorty.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbarMain.setupWithNavController(navController, appBarConfiguration)
+    }
+}
