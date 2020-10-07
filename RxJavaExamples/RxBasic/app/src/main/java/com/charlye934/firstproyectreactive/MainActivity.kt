76356070117d6d3a0bf0c57274eaf20f92cb2a52
charlye934 <charlye934@gmail.com>
2020-10-06 22:25:02 -0500

    first proyects rxjava

---
+++ RxJavaExamples/RxBasic/app/src/main/java/com/charlye934/firstproyectreactive/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.charlye934.firstproyectreactive
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupView()
+    }
+
+    private fun setupView(){
+        btRX00Introduccion.setOnClickListener {
+            startActivity(Intent(this,RX00IntroActivity::class.java))
+        }
+    }
+}
