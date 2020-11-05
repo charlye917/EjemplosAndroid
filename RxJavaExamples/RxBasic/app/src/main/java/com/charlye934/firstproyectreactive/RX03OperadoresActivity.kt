28cd331a19d1ae35fa96fd1aa383790c5f6950c7
charlye934 <charlye934@gmail.com>
2020-11-04 22:20:49 -0600

    pull

--- RxJavaExamples/RxBasic/app/src/main/java/com/charlye934/firstproyectreactive/RX03OperadoresActivity.kt
+++ RxJavaExamples/RxBasic/app/src/main/java/com/charlye934/firstproyectreactive/RX03OperadoresActivity.kt
@@ -26,7 +26,97 @@ class RX03OperadoresActivity : AppCompatActivity() {
         //probarInterval()
         //probarCreateLargaDuracion()
         //probarMap()
-        probarFlatMap()
+        //probarFlatMap()
+        //probarScan()
+        //probarWindow()
+        probarElementAt()
+    }
+
+    private fun probarElementAt(){
+        Log.d("TAG1", "----------------ElementAt----------------")
+        val numeroObservable = Observable.just(1,2,3,4,5,6,7,8,9,)
+        numeroObservable
+            .elementAt(7)
+            .subscribe({
+               Log.d("TAG1","OnNext: $it")
+            },{})
+    }
+
+    private fun probarDistinct(){
+        Log.d("TAG1", "----------------Distinct----------------")
+        val numeroObservable = Observable.just(1, 2, 3, 2, 5, 5, 1, 3, 9)
+        numeroObservable
+            .distinct()
+            .subscribe({
+                Log.d("TAG1", "onNExt: $it")
+            }, {
+
+            })
+    }
+
+    /*private fun probarDebounce() {
+        Log.d("TAG1", "----------------Debounce----------------")
+        val observable = RxTe.textChanges(etQuery)
+            .debounce(500, TimeUnit.MILLISECONDS)
+            .map { e -> e.toString() }
+            .subscribe { e ->
+                Log.d("TAG1", "onNext - String de Busqueda: $e")
+                tvQuery.setText("Query: $e")
+            } as Observable<String>
+    }*/
+
+    private fun probarWindow(){
+        Log.d("TAG1", "----------------Window----------------")
+        val observableObservable = Observable
+            .range(1, 150)
+            .window(3)
+        observableObservable
+            .subscribe({
+                Log.d("TAG1", "siguiente ventana")
+                it.subscribe({
+                    Log.d("TAG1", "item en ventana")
+                }, {
+                    Log.d("TAG1", "ERROR")
+                })
+            }, {
+                Log.d("TAG1", "ERROR")
+            })
+    }
+
+    private fun probarScan(){
+        Log.d("TAG1", "----------------Scan----------------")
+        Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9)
+            .scan { p0, p1 -> p0 + p1 }
+            .subscribe({
+                Log.d("TAG1", "onNext: $it")
+            }, {
+                Log.d("TAG1", "onError: $it")
+            })
+    }
+
+    private fun probarGroupBy(){
+        Log.d("TAG1", "----------------GroupBy----------------")
+        val numerosObservable = Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9)
+        val groupObservableObservable = numerosObservable
+            .groupBy {
+                if(it%2 == 0)
+                    return@groupBy "PAR"
+                else
+                    return@groupBy "IMPAR"
+            }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+
+        groupObservableObservable
+            .subscribe({
+                if (it.key.equals("PAR"))
+                    Log.d("TAG1", "es Par: $it")
+                else
+                    Log.d("TAG1", "es Impar: $it")
+            }, {
+                Log.d("TAG1", "onError: $it")
+            })
+
     }
 
     private fun probarFlatMap(){
@@ -34,7 +124,7 @@ class RX03OperadoresActivity : AppCompatActivity() {
         Observable
             .just("item2")
             .flatMap {
-                Log.d("TAG1", "inside the flapMat$s")
+                Log.d("TAG1", "inside the flapMat$it")
                 return@flatMap Observable.just("$it 1, $it 2, $it 3")
             }
             .subscribe({
