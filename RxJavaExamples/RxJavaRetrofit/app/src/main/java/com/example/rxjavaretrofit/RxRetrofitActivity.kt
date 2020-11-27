3ff865c82a637daccda68e0ed7a148086f26183f
Carlos Arteaga <carlos.arteaga@bancoazteca.com>
2020-11-26 18:05:56 -0600

    add rx java

--- RxJavaExamples/RxJavaRetrofit/app/src/main/java/com/example/rxjavaretrofit/RxRetrofitActivity.kt
+++ RxJavaExamples/RxJavaRetrofit/app/src/main/java/com/example/rxjavaretrofit/RxRetrofitActivity.kt
@@ -1,17 +1,22 @@
 package com.example.rxjavaretrofit
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.rxjavaretrofit.adapter.RepositoryAdapter
 import com.example.rxjavaretrofit.api.WebService
 import com.example.rxjavaretrofit.model.GithubRepo
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_rx_retrofit.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.function.Consumer
 
 class RxRetrofitActivity : AppCompatActivity() {
 
@@ -24,7 +29,39 @@ class RxRetrofitActivity : AppCompatActivity() {
         setContentView(R.layout.activity_rx_retrofit)
 
         setUpView()
-        sinRxJava()
+        //sinRxJava()
+        conRxLambda()
+    }
+
+    private fun conRx(){
+        compositeDisposable.add(
+            WebService
+                .createService()
+                .getReposForUserRx("JakeWharton")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(@RequiresApi(Build.VERSION_CODES.N)
+                object : Consum er<List<GithubRepo>>(){
+
+                })
+        )
+    }
+
+    private fun conRxLambda(){
+        compositeDisposable.add(
+            WebService
+                .createService()
+                .getReposForUserRx("JakeWharton")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    {
+                        adapter.setData(it)
+                    },{
+                        Log.d("TAG1","Error: ${it.printStackTrace()}")
+                    }
+                )
+        )
     }
 
     private fun sinRxJava(){
