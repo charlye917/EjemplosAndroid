ed3eb988796fdf0118afaab92514f5e64dc30356
Carlos Arteaga <carlos.arteaga@bancoazteca.com>
2021-01-11 10:09:20 -0600

    finish proyect retrofit

--- JetPack/RetrofitDemo/app/src/main/java/com/charlye934/retrofitdemo/AlbumService.kt
+++ JetPack/RetrofitDemo/app/src/main/java/com/charlye934/retrofitdemo/AlbumService.kt
@@ -1,11 +1,21 @@
 package com.charlye934.retrofitdemo
 
 import com.charlye934.retrofitdemo.model.Album
+import com.charlye934.retrofitdemo.model.AlbumItem
 import retrofit2.Response
-import retrofit2.http.GET
+import retrofit2.http.*
 
 
 interface AlbumService {
     @GET("/albums")
     suspend fun getAlbums(): Response<Album>
+
+    @GET("/albums")
+    suspend fun getSortedAlbums(@Query("userId") userId: Int): Response<Album>
+
+    @GET("/albums/{id}")
+    suspend fun getAlbum(@Path(value = "id") id: String): Response<AlbumItem>
+
+    @POST("/albums")
+    suspend fun uploadAlbum(@Body album:AlbumItem): Response<AlbumItem>
 }
--- JetPack/RetrofitDemo/app/src/main/java/com/charlye934/retrofitdemo/MainActivity.kt
+++ JetPack/RetrofitDemo/app/src/main/java/com/charlye934/retrofitdemo/MainActivity.kt
@@ -2,10 +2,82 @@ package com.charlye934.retrofitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import com.charlye934.retrofitdemo.model.Album
+import com.charlye934.retrofitdemo.model.AlbumItem
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var retService: AlbumService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       retService = RetrofitInstance
+            .getRetrofitInstance()
+            .create(AlbumService::class.java)
+
+        getRequestWithQueryParameters()
+        //getRequestWithPathParameters()
+        //uploadAlbums()
+    }
+
+    private fun getRequestWithQueryParameters(){
+
+        val responseLiveData: LiveData<Response<Album>> = liveData {
+            val response = retService.getSortedAlbums(3)
+            emit(response)
+        }
+
+        responseLiveData.observe(this, Observer {
+            val albumList = it.body()?.listIterator()
+            if(albumList != null){
+                while (albumList.hasNext()){
+                    val albumsItem = albumList.next()
+                    val result = " " + "Album title: ${albumsItem.title}" + " \n" +
+                            " " + " Album id: ${albumsItem.id}"  + "\n" +
+                            " " + " Album userId: ${albumsItem.userId}" + "\n\n"
+                    Log.d("__TAG",albumsItem.title)
+                    textView.append(result)
+                }
+            }
+        })
+    }
+
+    private fun getRequestWithPathParameters(){
+        //path parameters example
+        val pathResponse: LiveData<Response<AlbumItem>> = liveData {
+            val respose = retService.getAlbum("3")
+            emit(respose)
+        }
+
+        pathResponse.observe(this, Observer {
+            val albumList = it.body()?.title
+            Toast.makeText(this, "$albumList", Toast.LENGTH_SHORT).show()
+        })
+    }
+
+    private fun uploadAlbums(){
+        val albums = AlbumItem(0, "Mytitle", 1)
+
+        val postResponse: LiveData<Response<AlbumItem>> = liveData {
+            val response = retService.uploadAlbum(albums)
+            emit(response)
+        }
+
+        postResponse.observe(this, Observer {
+            val albumList = it.body()
+
+            val result = " " + "Album title: ${albumList!!.title}" + " \n" +
+                    " " + " Album id: ${albumList.id}"  + "\n" +
+                    " " + " Album userId: ${albumList.userId}" + "\n\n"
+        })
     }
 }
