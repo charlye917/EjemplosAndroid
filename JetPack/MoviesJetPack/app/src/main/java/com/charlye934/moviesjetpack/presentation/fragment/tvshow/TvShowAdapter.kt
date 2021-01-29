4b6b1b6cf16f29ebd80343506dabbdf134079b5c
Carlos Alberto Arteaga Lira <charlye934@MacBook-Pro-de-Carlos.local>
2021-01-28 21:40:17 -0600

    add tv

---
+++ JetPack/MoviesJetPack/app/src/main/java/com/charlye934/moviesjetpack/presentation/fragment/tvshow/TvShowAdapter.kt
@@ -0,0 +1,52 @@
+package com.charlye934.moviesjetpack.presentation.fragment.tvshow
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.charlye934.moviesjetpack.R
+import com.charlye934.moviesjetpack.data.model.tvshow.TvShow
+import com.charlye934.moviesjetpack.databinding.ListItemBinding
+
+class TvShowAdapter : RecyclerView.Adapter<TvShowAdapter.MyViewHolderTvShow>() {
+
+    private val tvShowList = ArrayList<TvShow>()
+
+    fun setData(tvShows: List<TvShow>){
+        tvShowList.clear()
+        tvShowList.addAll(tvShows)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolderTvShow {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding: ListItemBinding = DataBindingUtil.inflate(
+                layoutInflater,
+                R.layout.list_item,
+                parent,
+                false
+        )
+        return MyViewHolderTvShow(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolderTvShow, position: Int) {
+        holder.bind(tvShowList[position])
+    }
+
+    override fun getItemCount(): Int = tvShowList.size
+
+    class MyViewHolderTvShow(private val binding: ListItemBinding):
+        RecyclerView.ViewHolder(binding.root){
+
+        fun bind(tvShow: TvShow){
+            binding.titleTextView.text = tvShow.name
+            binding.descriptionTextView.text = tvShow.overview
+            val posterUrl = "https://image.tmdb.org/t/p/w500" + tvShow.posterPath
+            Glide.with(binding.imageView.context)
+                    .load(posterUrl)
+                    .into(binding.imageView)
+        }
+    }
+
+}
--- JetPack/MoviesJetPack/app/src/main/java/com/charlye934/moviesjetpack/presentation/fragment/tvshow/TvShowFragment.kt
+++ JetPack/MoviesJetPack/app/src/main/java/com/charlye934/moviesjetpack/presentation/fragment/tvshow/TvShowFragment.kt
@@ -1,4 +1,4 @@
-package com.charlye934.moviesjetpack.presentation.fragment
+package com.charlye934.moviesjetpack.presentation.fragment.tvshow
 
 import android.content.Context
 import android.os.Bundle
@@ -7,7 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.charlye934.moviesjetpack.R
 import com.charlye934.moviesjetpack.databinding.FragmentTvShowBinding
 import com.charlye934.moviesjetpack.presentation.di.Injector
@@ -17,9 +19,11 @@ import javax.inject.Inject
 
 class TvShowFragment : Fragment() {
 
-
+    @Inject
+    lateinit var factory: TvShowViewModelFactory
     private lateinit var tvShowViewModel: TvShowViewModel
     private lateinit var binding: FragmentTvShowBinding
+    private lateinit var adapterTvShow: TvShowAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,7 +36,33 @@ class TvShowFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        tvShowViewModel = ViewModelProvider(this, factory)
+                .get(TvShowViewModel::class.java)
 
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        adapterTvShow = TvShowAdapter()
+        binding.tvRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = adapterTvShow
+        }
+
+        displayPopularMovies()
+    }
+
+    private fun displayPopularMovies() {
+        binding.tvProgressBar.visibility = View.VISIBLE
+        val responseLiveData = tvShowViewModel.getTvShow()
+        responseLiveData.observe(this, Observer {
+            if(it != null){
+                adapterTvShow.setData(it)
+                binding.tvProgressBar.visibility = View.GONE
+            }else{
+                binding.tvProgressBar.visibility = View.GONE
+            }
+        })
     }
 
     override fun onAttach(context: Context) {
