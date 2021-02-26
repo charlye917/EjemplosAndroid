2e5700525f70f28cac0bcbea0593fc0219abaccf
Carlos Arteaga <carlos.arteaga@bancoazteca.com>
2021-02-26 13:39:43 -0600

    changes

--- Firebase/Chat/app/src/main/java/com/example/chat/LoginActivity.kt
+++ Firebase/Chat/app/src/main/java/com/example/chat/LoginActivity.kt
@@ -4,21 +4,51 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.chat.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
+    private val mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setOnClick()
     }
 
     private fun setOnClick() {
-        buttonLoginGoSignUp.setOnClickListener { goToSignUp() }
-        textViewForgotPassword.setOnClickListener { goToForgotPassword() }
+        with(binding){
+            buttonLoginGoSignUp.setOnClickListener { goToSignUp() }
+            textViewForgotPassword.setOnClickListener { goToForgotPassword() }
+            buttonLogin.setOnClickListener { signInLogin() }
+        }
+    }
+
+    private fun signInLogin(){
+        val email = binding.editTextEmail.text.toString().trim()
+        val password = binding.editTextPassword.text.toString().trim()
+
+        if(validateData(email, password)){
+            mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        goToMain()
+                    } else {
+                        Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }else{
+            Toast.makeText(baseContext, "Favor de llenar los campos", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun validateData(email: String, password: String): Boolean{
+        return email.isNotEmpty() && password.isNotEmpty()
     }
 
     private fun goToSignUp() {
@@ -28,6 +58,12 @@ class LoginActivity : AppCompatActivity() {
 
     private fun goToForgotPassword(){
         val intent = Intent(this, ForgotPasswordActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun goToMain(){
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
     }
 }
--- Firebase/Chat/app/src/main/java/com/example/chat/SignUpActivity.kt
+++ Firebase/Chat/app/src/main/java/com/example/chat/SignUpActivity.kt
@@ -5,60 +5,65 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.example.chat.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
     private val mAunt: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private lateinit var binding: ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setOnClickListener()
     }
 
     private fun setOnClickListener(){
-        buttonSignUp.setOnClickListener { checkData() }
-        buttonGoLoginSignUp.setOnClickListener { goToLogin() }
+        with(binding){
+            buttonSignUp.setOnClickListener { checkData() }
+            buttonGoLoginSignUp.setOnClickListener { goToLogin() }
+        }
     }
 
     private fun checkData(){
-        val currentUser = mAunt.currentUser
-        val user = editTextEmailSignUp.text.toString()
-        val password = editTextPasswordSignUp.text.toString()
-        val confirmPass = editTextConfirmPasswordSignUp.toString()
+        val email = binding.editTextEmailSignUp.text.toString().trim()
+        val password = binding.editTextPasswordSignUp.text.toString().trim()
 
-        if(user.isNotEmpty() && password.isNotEmpty() && confirmPass.isNotEmpty() && password == confirmPass){
-            if(currentUser == null){
-                Toast.makeText(this, "User is not logger", Toast.LENGTH_SHORT).show()
-                createAccount(user, password)
-            }else{
-                Toast.makeText(this, "User is logger", Toast.LENGTH_SHORT).show()
-            }
+        if(validateData(email, password)){
+            createAccount(email, password)
         }else{
             Toast.makeText(this, "Favor de llenar los campos", Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun createAccount(email:String, password:String){
+    private fun createAccount(email: String, password: String){
         mAunt.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    Log.d("__TAG", "createUserWithEmail:success")
-                    val user = mAunt.currentUser
+                    mAunt.currentUser!!.sendEmailVerification().addOnCompleteListener(this) {
+                        Toast.makeText(this, "An email has been sent to you. Please, confirm before sign in.", Toast.LENGTH_SHORT).show()
+                        goToLogin()
+                        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                    }
                 } else {
-                    Log.w("__TAG", "createUserWithEmail:failure", task.exception)
                     Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
-                    //updateUI(null)
                 }
             }
     }
 
     private fun goToLogin(){
         val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
+    }
+
+    private fun validateData(email: String, password: String): Boolean{
+        return email.isNotEmpty()
+                && password.isNotEmpty()
+                && password == binding.editTextConfirmPasswordSignUp.text.toString()
     }
 }
